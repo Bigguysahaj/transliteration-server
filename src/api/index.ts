87802c33216948cdc381ff61cee101db5db01ea1
bigguysahaj <singhsahaj2001@gmail.com>
2024-01-10 05:58:47 +0530

    add "/" api path

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,16 +1,16 @@
 import express from 'express';
 
-import {MessageResponse , MessageRequest} from '../interfaces/MessageResponse';
+import {MessageResponse} from '../interfaces/MessageResponse';
 // import emojis from './emojis';
 import transliterate from './transliterate';
 
 const router = express.Router();
 
-// router.get<{}, MessageResponse>('/', (req, res) => {
-//   res.json({
-//     message: 'API - 👋🌎🌍🌏',
-//   });
-// });
+router.get<{}, MessageResponse>('/', (req, res) => {
+  res.json({
+    message: ' Transliteration API - 👋🌎🌍🌏',
+  });
+});
 
 // router.use('/emojis', emojis);
 router.use('/transliterate', transliterate);
